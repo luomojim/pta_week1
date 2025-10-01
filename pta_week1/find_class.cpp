8503555a6aadef7bb355bc3aca5fe8a05a99db9e
luomo233 <157657883@qq.com>
2025-10-01 16:04:25 +0800

    sync_2

---
+++ pta_week1/find_class.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class class_name
+{
+	public:
+		int day;
+
+		void set_day(int day)
+		{
+			this->day = day;
+		}
+
+		bool range(void)
+		{
+			if (day >= 1 && day <= 5)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		string process(void)
+		{
+			if (range())
+			{
+				switch (day)
+				{
+				case(1):
+					return "《C语言程序设计》";
+					break;
+				case(2):
+					return "没课";
+					break;
+				case(3):
+					return "《数据结构》";
+					break;
+				case(4):
+					return "没课";
+					break;
+				case(5):
+					return "《python语言程序设计》";
+					break;
+				}
+			}
+
+			return "输入数字需要在1~5之间";
+		}
+		
+};
+
+
+int find_class ()
+{
+	int day = 0;
+	class_name class1;
+	cin >> day;
+	class1.set_day(day);
+
+	cout << class1.process() << endl;
+
+	return 0;
+}
--- pta_week1/how_is_day_2.cpp
+++ pta_week1/how_is_day_2.cpp
@@ -2,16 +2,62 @@
 
 using namespace std;
 
-
-
-
-
-
-
-int main()
+class week
 {
+	public:
+		long long day = 6;
+
+		void set_day(int d)
+		{
+			day += d;
+			day %= 7;
+			while (day < 0)
+			{
+				day += 7;
+			}
+		}
+
+		string process(void)
+		{
+			switch (day)
+			{
+			case(1):
+				return "星期一";
+				break;
+			case(2):
+				return "星期二";
+				break;
+			case(3):
+				return "星期三";
+				break;
+			case(4):
+				return "星期四";
+				break;
+			case(5):
+				return "星期五";
+				break;
+			case(6):
+				return "星期六";
+				break;
+			case(0):
+				return "星期日";
+				break;
+			}
+		}
+};
 
 
+
+
+
+int how_is_day_2()
+{
+	long long d = 0;
+	week w;
+	cin >> d;
+	w.set_day(d);
+
+	cout << w.process() << endl;
 
 	return 0;
 }
